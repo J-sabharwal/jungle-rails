cefb928adbb63ab345897b885c7df3811d3bc773
Jaspinder Sabharwal <jas@jaspinder.net>
2020-04-30 06:36:28 +0000

    added dashboard features

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV["ADMIN_USERNAME"], :password => ENV["ADMIN_PASSWORD"]
   
   def show
+    @category = Category.all
+    @products = Product.all
   end
+  helper_method :show
+
 end
