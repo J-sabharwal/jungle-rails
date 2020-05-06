2589c6db934944bc53472715ecda46a8e83b49df
Jaspinder Sabharwal <jas@jaspinder.net>
2020-05-06 23:45:18 +0000

    removal of whitespaces

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  
-  get 'about/' => 'about#show'
-  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   
@@ -24,9 +21,9 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  get 'about/' => 'about#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
